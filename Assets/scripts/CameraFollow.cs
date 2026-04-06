229931b8bbb01bfb2531b9158f6bf974894440b8
Felicia-Hardy <2504040223@ogr.kent.edu.tr>
2026-04-07 01:04:18 +0300

    camera angle
    The camera angle changed. The ground grew larger.

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
-    public Vector3 offset;
+    public Vector3 offset = new Vector3(0, 15, 0);
 
     void LateUpdate()
     {
         transform.position = player.position + offset;
+        transform.rotation = Quaternion.Euler(90f, 0f, 0f); // kuþ bakýþý
     }
 }
